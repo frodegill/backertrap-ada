2182a7fccf136d4aabd5d44bfd0adc0f48938454
Frode Roxrud Gill <frodegill@gmail.com>
2015-03-16 18:29:43 +0100

    Added FreePixel (12px) font

---
+++ fonts/FreePixel/font.h
@@ -0,0 +1,21 @@
+#ifndef _FREEPIXEL_FONT_12_H_
+#define _FREEPIXEL_FONT_12_H_
+
+// Copyright (C) 2015  Frode Roxrud Gill
+// See LICENSE file for license
+//
+// Font-data from <URL: http://www.dafont.com/free-pixel.font?fpp=100&l[]=10&l[]=1 >
+
+#include "../../defines.h"
+#include "../font.h"
+
+
+class FreePixelFont12 : public Font
+{
+public:
+  FreePixelFont12();
+  ~FreePixelFont12();
+
+};
+
+#endif //_FREEPIXEL_FONT_12_H_
