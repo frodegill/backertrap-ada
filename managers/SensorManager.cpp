f1279c3f1d4b3aab587f44a43c0384bcc6636f50
Frode Roxrud Gill <frodegill@gmail.com>
2015-03-17 15:48:57 +0100

    No need for precompiled headers

--- managers/SensorManager.cpp
+++ managers/SensorManager.cpp
@@ -1,11 +1,6 @@
 
 // Copyright (C) 2015  Frode Roxrud Gill
-// See LICENSE file for license
-
-#ifdef PRECOMP
-# include "../pch.h"
-#else
-#endif
+// See LICENSE file for GPLv3 license
 
 #include "SensorManager.h"
 
