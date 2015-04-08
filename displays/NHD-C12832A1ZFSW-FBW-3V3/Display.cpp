bfab5859cb34369157c2265ee845a5bf5f6188e9
Frode Roxrud Gill <frodegill@gmail.com>
2015-04-08 22:44:09 +0200

    Who decided st7565r should have a paged, tilted pixel buffer!?!

--- displays/NHD-C12832A1ZFSW-FBW-3V3/Display.cpp
+++ displays/NHD-C12832A1ZFSW-FBW-3V3/Display.cpp
@@ -58,28 +58,63 @@ bool DisplayNHDC12832A1ZFSWFBW3V3::Init()
 
 void DisplayNHDC12832A1ZFSWFBW3V3::SetBacklightStatus(Display::BacklightStatus status)
 {
-    ioport_set_pin_level(NHD_C12832A1Z_BACKLIGHT, Display::OFF != status);
+	ioport_set_pin_level(NHD_C12832A1Z_BACKLIGHT, Display::OFF != status);
 }
 
 void DisplayNHDC12832A1ZFSWFBW3V3::ClearFramebuffer()
 {
-    U16 i;
-    for (i=0; i<(NHDC12832A1ZFSWFBW3V3_WIDTH*NHDC12832A1ZFSWFBW3V3_HEIGHT/8); i++)
-    {
-        g_DisplayNHDC12832A1ZFSWFBW3V3_framebuffer_p[i] = 0;
-    }
+	U16 i;
+	for (i=0; i<(NHDC12832A1ZFSWFBW3V3_WIDTH*NHDC12832A1ZFSWFBW3V3_HEIGHT/8); i++)
+	{
+		g_DisplayNHDC12832A1ZFSWFBW3V3_framebuffer_p[i] = 0;
+	}
 }
 
 void DisplayNHDC12832A1ZFSWFBW3V3::SwapBuffers()
 {
-#if 0
-    U8 i;
-    for (i=0; i<NHDC12832A1ZFSWFBW3V3_PAGES; i++)
-    {
-        st7565r_set_page_address(i);
-        st7565r_set_column_address(0);
-        gfx_mono_st7565r_put_page(&g_DisplayNHDC12832A1ZFSWFBW3V3_framebuffer_p[i*NHDC12832A1ZFSWFBW3V3_WIDTH],
-                                  i, 0, NHDC12832A1ZFSWFBW3V3_WIDTH);
-    }
-#endif
+	U16 min_x_dirty, min_y_dirty,max_x_dirty,max_y_dirty;
+	GetDirty(min_x_dirty, min_y_dirty, max_x_dirty, max_y_dirty);
+	if (min_x_dirty>max_x_dirty || min_y_dirty>max_y_dirty)
+		return;
+
+	U8 min_y_page = min_y_dirty>>3;
+	U8 max_y_page = max_y_dirty>>3;
+	U8 min_x_byte = min_x_dirty>>3;
+	U8 max_x_byte = max_x_dirty>>3;
+	U8 page, x_byte;
+	U8 data[8];
+	U8 i;
+	U8 x, y, fb_data;
+	for (page=min_y_page; page<=max_y_page; page++)
+	{
+		st7565r_set_page_address(page);
+		st7565r_set_column_address(min_x_byte<<3);
+
+		for (x_byte=min_x_byte; x_byte<=max_x_byte; x_byte++)
+		{
+			for (i=0; i<8; i++)
+			{
+				data[i] = 0;
+			}
+
+			for (y=0; y<8; y++)
+			{
+				fb_data = g_DisplayNHDC12832A1ZFSWFBW3V3_framebuffer_p[(page*8 + y)*NHDC12832A1ZFSWFBW3V3_BYTES_PER_ROW + x_byte];
+				for (x=0; x<8; x++)
+				{
+					if (fb_data & (1<<(7-x)))
+					{
+						data[x] |= 1<<(7-y);
+					}
+				}
+			}
+
+			for (i=0; i<8; i++)
+			{
+				st7565r_write_data(data[i]);
+			}
+		}
+	}
+
+	ResetDirty();
 }
