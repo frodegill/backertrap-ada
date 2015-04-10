a233bffe194c153c04276eb0a382b2ee1c0d266f
Frode Roxrud Gill <frodegill@gmail.com>
2015-04-10 18:16:11 +0200

    Removed unused code

--- gfx_mono_generic.h
+++
@@ -1,9 +0,0 @@
-#ifndef _GFX_MONO_GENERIC_HACK_H_
-#define _GFX_MONO_GENERIC_HACK_H_
-
-// Copyright (C) 2015  Frode Roxrud Gill
-// See LICENSE file for GPLv3 license
-
-//Make the code copied from Atmel ASF compile with as few modifications as possible
-
-#endif // _GFX_MONO_GENERIC_HACK_H_
