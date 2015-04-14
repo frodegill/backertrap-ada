8ed05f2e166f1dd6b3061fedd6d67570ccf586cf
Frode Roxrud Gill <frodegill@gmail.com>
2015-04-14 09:14:12 +0200

    Added infinite loop so application will not exit

--- BackertrapAdaApp.cpp
+++ BackertrapAdaApp.cpp
@@ -30,12 +30,76 @@ bool BackertrapAdaApp::Init()
     return false;
 	}
 
+#if 0
+	touch_init();
+	adc_sensors_init();
+
+	// Initialize USB CDC class
+	cdc_start();
+
+
+	// Display a splash screen showing button functions
+	button_splash();
+
+	// Set timezone from EEPROM or to a default value
+	timezone_init();
+
+	/* Main loop. Read keyboard status and pass input to menu system.
+	 * When an element has been selected in the menu, it will return the
+	 * index of the element that should be run. This can be an application
+	 * or another menu.
+	 */
+	while (true) {
+
+		// (re)initialize menu system
+		gfx_mono_menu_init(&main_menu);
+
+		do {
+			do {
+				keyboard_get_key_state(&input);
+			// Wait for key release
+			} while (input.type != KEYBOARD_RELEASE);
+
+			// Send key to menu system
+			menu_status = gfx_mono_menu_process_key(&main_menu, input.keycode);
+		// Wait for something useful to happen in the menu system
+		} while (menu_status == GFX_MONO_MENU_EVENT_IDLE);
+
+		switch (menu_status) {
+		case 0:
+			ntc_sensor_application();
+			break;
+		case 1:
+			lightsensor_application();
+			break;
+		case 2:
+			production_date_application();
+			break;
+		case 3:
+			date_time_application();
+			break;
+		case 4:
+			// Toggle LCD Backlight
+			gpio_toggle_pin(NHD_C12832A1Z_BACKLIGHT);
+			break;
+		case GFX_MONO_MENU_EVENT_EXIT:
+			// Fall trough to default and show button splash
+		default:
+			button_splash();
+			break;
+		};
+	}
+#endif
   return true;
 }
 
 void BackertrapAdaApp::Run()
 {
 	m_page_manager.PushPage(Page::BOOTPAGE);
+	
+	while(true)
+	{
+	}
 }
 
 /********************************/
