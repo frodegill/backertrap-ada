fc66ecfb0371d3543d515f8856d7e98066b5585c
Frode Roxrud Gill <frodegill@gmail.com>
2015-05-12 09:25:03 +0200

    Added manager for GPIO

---
+++ managers/GPIOManager.cpp
@@ -0,0 +1,14 @@
+
+// Copyright (C) 2015  Frode Roxrud Gill
+// See LICENSE file for GPLv3 license
+
+#include "GPIOManager.h"
+
+
+GPIOManager::GPIOManager()
+{
+}
+
+GPIOManager::~GPIOManager()
+{
+}
---
+++ managers/GPIOManager.h
@@ -0,0 +1,18 @@
+#ifndef _GPIO_MANAGER_H_
+#define _GPIO_MANAGER_H_
+
+// Copyright (C) 2015  Frode Roxrud Gill
+// See LICENSE file for GPLv3 license
+
+#include "../defines.h"
+
+
+class GPIOManager
+{
+public:
+	GPIOManager();
+	~GPIOManager();
+
+};
+
+#endif // _GPIO_MANAGER_H_
